a88f0f85e91e353113cc7cc3a3bd3273afda2641
HcwXd <e04dl47878@gmail.com>
2018-07-02 09:03:49 +0800

    Setting basic router for pages render

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('explore/explore');
 });
+
+router.get('/explore', function (req, res, next) {
+  res.render('explore/explore');
+});
+
+router.get('/login', function (req, res, next) {
+  res.render('login/login');
+});
+
+router.get('/profile', function (req, res, next) {
+  res.render('profile/profile');
+});
+
+router.get('/signup', function (req, res, next) {
+  res.render('signup/signup');
+});
+
+router.get('/publish', function (req, res, next) {
+  res.render('publish/publish');
+});
+
 
 module.exports = router;
