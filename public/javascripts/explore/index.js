b8e50319619e2d16d4f01c0d6b5d0104f6e3964b
HcwXd <e04dl47878@gmail.com>
2018-07-02 23:27:49 +0800

    Explore page: Add animation to show song list

--- public/javascripts/explore/index.js
+++ public/javascripts/explore/index.js
@@ -468,14 +468,36 @@ let posts_wrap = document.querySelector('.posts_wrap');
 for (let i = 0; i < fivePlaylistInfo.length; i++) {
     let post = document.createElement('div');
     post.className = 'post';
+    let like = 0;
+    fivePlaylistInfo[i].songList.forEach(song => {
+        like += song.like;
+    });
     post_html = `
-    <div class="owner_info">
-        <img class="owner_avatar" src="${fivePlaylistInfo[i].avatar}">
-        <div class="owner_name">${fivePlaylistInfo[i].ownerName}</div>
+    <div class="song_list">
+        <div class="song_list_des">播放清單</div>
+        <div class="song_info">
+        <div class="song_name">既視感 - 不規則鐘擺</div>
+        </div>
+        <div class="song_info">
+        <div class="song_name">大象體操ElephantGym _ 中途Midway【Official Music Video】</div>
+        </div>
+        <div class="song_info">
+        <div class="song_name">tfvsjs - 七夕缺 (live version)</div>
+        </div>
+        <div class="song_info">
+        <div class="song_name">企鵝熊愛吃雞肉球 - 十七歲</div>
+        </div>
+    </div>
+    <div class="header">
+        <div class="owner_info">
+            <img class="owner_avatar" src="${fivePlaylistInfo[i].avatar}">
+            <div class="owner_name">${fivePlaylistInfo[i].ownerName}</div>
+        </div>
+        <div class="more_info">=</div>
     </div>
     <img class="playlist_cover" src="${fivePlaylistInfo[i].songList[0].cover}">
     <div class="playlist_stats">
-        <div class="like">♥${fivePlaylistInfo[i].songList[0].like}</div>
+        <div class="like">♥${like}</div>
         <div class="date">${fivePlaylistInfo[i].date}</div>
     </div>
     <div class="playlist_title">${fivePlaylistInfo[i].name}</div>
@@ -483,4 +505,22 @@ for (let i = 0; i < fivePlaylistInfo.length; i++) {
     </div>`
     post.innerHTML = post_html;
     posts_wrap.appendChild(post);
+};
+let more_infos = document.querySelectorAll('.more_info');
+more_infos.forEach(ho => ho.addEventListener('mouseout', hideSongList))
+more_infos.forEach(ho => ho.addEventListener('mouseenter', showSongList))
+
+function showSongList() {
+    const song_list = this.parentNode.previousSibling.previousSibling;
+    console.log(song_list);
+    song_list.style.transform = "rotate(0)";
+    song_list.style.left = "101%";
+    song_list.style.opacity = "1";
+}
+
+function hideSongList() {
+    const song_list = this.parentNode.previousSibling.previousSibling;
+    song_list.style.transform = "rotate(90deg)"
+    song_list.style.left = "100%"
+    song_list.style.opacity = "0";
 }
