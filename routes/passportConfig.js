15e2e137fe0a6884c9a41a646f1251cb8144bcae
HcwXd <e04dl47878@gmail.com>
2018-07-04 23:17:57 +0800

    Revise passsportConfig callbackURL

--- routes/passportConfig.js
+++ routes/passportConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
     CLIENT_ID: '404379326715324',
     CLIENT_SECRET: '427b7cfbcbc8f5e5ec8624e537923b41',
-    // callbackURL: 'http://localhost:3000/login/facebook/return'
-    callbackURL: 'https://playlist-explore.herokuapp.com/login/facebook/return'
+    callbackURL: 'http://localhost:3000/login/facebook/return'
+    // callbackURL: 'loc/login/facebook/return'
 }
