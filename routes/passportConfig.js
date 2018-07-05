373c48e18cff8b4ed0d7069b216e9a9b2bab2dba
蔡明宏 <tall154215452@caiminghongde-MacBook-Pro.local>
2018-07-05 19:12:46 +0800

    update client secret for local

--- routes/passportConfig.js
+++ routes/passportConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    CLIENT_ID: '404379326715324',
-    CLIENT_SECRET: '427b7cfbcbc8f5e5ec8624e537923b41',
+    CLIENT_ID: '251561765428847',
+    CLIENT_SECRET: '461918997804adb472c7ae80ee3a10c7',
     callbackURL: 'http://localhost:3000/login/facebook/return'
 }
