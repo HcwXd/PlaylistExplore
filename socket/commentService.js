ea07574a2a650eb16b3b35132af9296885da78b6
tall15421542 <b04703001@ntu.edu.tw>
2018-07-13 17:20:44 +0800

    fix delete comment

--- socket/commentService.js
+++ socket/commentService.js
@@ -1,10 +1,6 @@
-const { userTable,
-        commentTable,
-        songTable,
-        relationTable,
-        songListTable, } = require('./database');
+const { userTable, commentTable, songTable, relationTable, songListTable } = require('./database');
 
-function emitLatestComment(listOwnerToken, songIndex, listId){
+async function emitLatestComment(listOwnerToken, songIndex, listId) {
     songInfo = {
         token: listOwnerToken,
         songIndex: songIndex,
@@ -14,14 +10,14 @@ function emitLatestComment(listOwnerToken, songIndex, listId){
     socket.emit('getSongComment', comments);
 }
 
-function commentService(socket){
+function commentService(socket) {
     socket.on('getSongComment', async (songInfo) => {
         comments = await songTable.getCommentInfo(songInfo);
         socket.emit('getSongComment', comments);
     });
 
     socket.on('newComment', async (commentInfo) => {
-        if(commentInfo.commentContent){
+        if (commentInfo.commentContent) {
             commentInfo['commentToken'] = socket.handshake.session.token;
             await commentTable.createComment(commentInfo);
         }
@@ -31,7 +27,7 @@ function commentService(socket){
     socket.on('deleteComment', async (commentInfo) => {
         commentTable.deleteComment(commentInfo.commentIndex);
         emitLatestComment(commentInfo.listOwnerToken, commentInfo.songIndex, commentInfo.listId);
-    })
-};
+    });
+}
 
 module.exports = commentService;
