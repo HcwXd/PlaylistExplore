ac0ddd6739ffe6cd61e75fdff8e73426f3903aa3
HcwXd <e04dl47878@gmail.com>
2018-07-26 15:46:20 +0800

    Add config-lite to separate test and production env

--- routes/passportConfig.js
+++ routes/passportConfig.js
@@ -1,3 +1,5 @@
+const config = require('config-lite')(__dirname);
+
 module.exports = {
     // Offline
     /*
@@ -5,9 +7,13 @@ module.exports = {
     CLIENT_SECRET: '461918997804adb472c7ae80ee3a10c7',
     callbackURL: 'http://localhost:3000/login/facebook/return',
     */
-
     // Online
+    /*
     CLIENT_ID: '404379326715324',
     CLIENT_SECRET: '427b7cfbcbc8f5e5ec8624e537923b41',
     callbackURL: 'https://playlist-explore.herokuapp.com/login/facebook/return',
+    */
+    CLIENT_ID: config.CLIENT_ID,
+    CLIENT_SECRET: config.CLIENT_SECRET,
+    callbackURL: config.callbackURL,
 };
