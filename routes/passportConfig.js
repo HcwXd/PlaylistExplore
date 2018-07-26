3e38d0d975fe2fae8fab1d8905132307ad9bac67
HcwXd <e04dl47878@gmail.com>
2018-07-26 15:30:26 +0800

    Revise clientID to push onto heroku

--- routes/passportConfig.js
+++ routes/passportConfig.js
@@ -1,6 +1,13 @@
 module.exports = {
+    // Offline
+    /*
     CLIENT_ID: '251561765428847',
     CLIENT_SECRET: '461918997804adb472c7ae80ee3a10c7',
-    // callbackURL: 'http://localhost:3000/login/facebook/return',
+    callbackURL: 'http://localhost:3000/login/facebook/return',
+    */
+
+    // Online
+    CLIENT_ID: '404379326715324',
+    CLIENT_SECRET: '427b7cfbcbc8f5e5ec8624e537923b41',
     callbackURL: 'https://playlist-explore.herokuapp.com/login/facebook/return',
 };
