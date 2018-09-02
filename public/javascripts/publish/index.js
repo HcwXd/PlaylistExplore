0351ee005ef2f5ca901d67e401ac85e2dc6d6e88
HcwXd <e04dl47878@gmail.com>
2018-09-02 22:57:15 +0800

    Publish page: Put each des adding textarea to playlist

--- public/javascripts/publish/index.js
+++ public/javascripts/publish/index.js
@@ -130,10 +130,14 @@ function returnSongInfoNode(songName, songUrl) {
     song_delete_node.dataset.url = songUrl;
     song_delete_node.addEventListener('click', deleteSongFromPlaylist);
 
-    let song_des_wrap_node = document.createElement('input');
+    let song_des_wrap_node = document.createElement('textarea');
     song_des_wrap_node.className = 'song_des_wrap';
+    song_des_wrap_node.placeholder = '請輸入歌曲描述';
     song_des_wrap_node.url = songUrl;
     song_des_wrap_node.dataset.url = songUrl;
+
+    let des_connect_line_node = document.createElement('div');
+    des_connect_line_node.className = 'des_connect_line';
 
     let song_info_node = document.createElement('div');
     song_info_node.className = 'song_info';
@@ -143,6 +147,7 @@ function returnSongInfoNode(songName, songUrl) {
     song_info_node.draggable = true;
 
     song_info_node.appendChild(song_des_wrap_node);
+    song_info_node.appendChild(des_connect_line_node);
     song_info_node.appendChild(song_cover_node);
     song_info_node.appendChild(song_name_node);
     song_info_node.appendChild(song_delete_node);
@@ -392,19 +397,7 @@ function handleDrop(e) {
 }
 
 function handleDragEnd(e) {
-    console.log(this.firstChild.value);
     this.style.borderTop = '0px solid yellow';
-    bindValueAfterDragInput(this.firstChild, this.firstChild.value);
-}
-
-function bindValueAfterDragInput(node, value) {
-    console.log('bindValueAfterDragInput');
-    console.log({ node, value });
-    node.value = value;
-    setTimeout(() => {
-        console.log({ node, value });
-        node.value = value;
-    }, 1000);
 }
 
 // Compression of image
