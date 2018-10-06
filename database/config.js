5e6fee70fb6f8e2790b6f148cdd3c665000e06cf
tall15421542 <b04703001@ntu.edu.tw>
2018-10-06 20:09:28 +0800

    setup new database

--- database/config.js
+++ database/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    host: 'sql12.freemysqlhosting.net',
-    user: 'sql12246913',
-    password: 'FdR9sQddDH',
-    database: 'sql12246913',
-}
+    host: 'sql7.freemysqlhosting.net',
+    user: 'sql7260032',
+    password: 'vsypEgiF9j',
+    database: 'sql7260032',
+};
