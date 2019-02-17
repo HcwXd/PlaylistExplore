e02ee9aad9c3d28f19e9bea83c9a20fd7eb76b34
HcwXd <e04dl47878@gmail.com>
2019-02-16 18:30:17 -0800

    Fix indent

--- socket/ytAPIService.js
+++ socket/ytAPIService.js
@@ -1,11 +1,18 @@
-const { userTable, commentTable, songTable, relationTable, songListTable, likeTable } = require('./database');
+const {
+  userTable,
+  commentTable,
+  songTable,
+  relationTable,
+  songListTable,
+  likeTable,
+} = require('./database');
 
 const emitURLInfoArray = require('./songSearch');
 
 function ytAPIService(socket) {
-    socket.on('getSearchResults', async (URL) => {
-        await emitURLInfoArray(URL, socket);
-    });
+  socket.on('getSearchResults', async (URL) => {
+    await emitURLInfoArray(URL, socket);
+  });
 }
 
 module.exports = ytAPIService;
